00bfd41e2621186a50d79088c0e93f30ddfb11cb
Cameron Brown <cbrown@thinkallabout.co.uk>
2014-05-20 21:26:39 +0100

    Fixed template bug

---
+++ blot/path.py
@@ -0,0 +1,13 @@
+import os
+
+class path():
+	def __init__(self, path):
+		""" Set Blot working directory """
+		self.path = path
+		if os.path.exists(self.path) != True:
+			os.mkdir(self.path)
+
+		# Default dirs
+		self.images = path + os.path.join('images')
+		self.posts = path + os.path.join('posts')
+		self.templates = path + os.path.join('templates')
--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 from blot import Blot
 import os
 
-root_dir = os.getcwd() + '/src'
-scripts = root_dir + '/scripts'
+root_dir = os.getcwd() + '/src/'
 site = Blot(root_dir)
 
-site.path.images = root_dir + '/img'
-site.path.posts = root_dir + '/posts'
-site.path.templates = root_dir + '/templates'
+site.path.images = root_dir + '/img/'
+site.path.posts = root_dir + '/posts/'
+site.path.templates = root_dir + '/templates/'
 
-site.conf.output = root_dir + '/bin'
+site.conf.output = root_dir + '/bin/'
+
+print site.templates
 
 if __name__ == '__main__':
 	site.compile()
